6976bb35ba6b92ad97e16dcef00716747befdff7
wahid409 <wahid409@gmail.com>
2019-01-05 03:21:24 +0600

    configured environment

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require ('express')
 const app = express()
 
+const port = process.env.port || 5000
+
 app.use(express.static(__dirname + "/public"))
 
 app.get('/', (req, res) => {
     res.sendFile(index.html)
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('app running on port 3000')
 })
